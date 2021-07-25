a1263c74c7047bfe5d6c59dfc9811be0dc917da0
Roei Sabag <roeisabag@coralogix.com>
2021-07-25 13:50:41 +0300

    updates

---
+++ src/UiaDriverServer/Dto/Location.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace UiaDriverServer.Dto
+{
+    [DataContract]
+    internal class Location
+    {
+        [DataMember]
+        public int Top { get; set; }
+        
+        [DataMember]
+        public int Left { get; set; }
+    }
+}
