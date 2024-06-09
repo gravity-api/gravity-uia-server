7e914dc1a620f22f88ef8adf6ca151c79870ddc2
Ofer Lifshes <lifshesofer@gmail.com>
2024-06-09 08:39:03 +0300

    Added 'Click' extension method for Element

--- src/UiaWebDriverServer.Extensions/ElementExtensions.cs
+++ src/UiaWebDriverServer.Extensions/ElementExtensions.cs
@@ -6,12 +6,28 @@ namespace UiaWebDriverServer.Extensions
 {
     public static class ElementExtensions
     {
+        public static Element Click(this Element element)
+        {
+            return element.Click(1.0D);
+        }
+
+        public static Element Click(this Element element, double scaleRatio)
+        {
+            if(element?.UIAutomationElement == null)
+            {
+                element.NativeClick(scaleRatio);
+            }
+            else
+            {
+                element.UIAutomationElement.Click(scaleRatio);
+            }
+            return element;
+        }
 
         public static void NativeClick(this Element element, double scaleRatio)
         {
             var point = InvokeGetClickablePoint(scaleRatio, element);
             ExternalMethods.SetPhysicalCursorPos(point.XPos, point.YPos);
-
 
             InvokeNativeClick();
         }
@@ -36,7 +52,28 @@ namespace UiaWebDriverServer.Extensions
         }
 
         /// <summary>
-        /// Calculates the clickable point relative to an uiElement based on alignment and offsets.
+        /// Try to get a mouse click-able point of the element.
+        /// </summary>
+        /// <param name="element">The Element to get click-able point for.</param>
+        /// <returns>A ClickablePoint object.</returns>
+        public static ClickablePoint GetClickablePoint(this Element element)
+        {
+            return InvokeGetClickablePoint(scaleRatio: 1.0D, element);
+        }
+
+        /// <summary>
+        /// Try to get a mouse click-able point of the element.
+        /// </summary>
+        /// <param name="element">The Element to get click-able point for.</param>
+        /// <param name="scaleRatio">The screen scale ratio e.g., if the scale ratio is 250% this number will be 2.5, if 150% it will be 1.5, etc.</param>
+        /// <returns>A ClickablePoint object.</returns>
+        public static ClickablePoint GetClickablePoint(this Element element, double scaleRatio)
+        {
+            return InvokeGetClickablePoint(scaleRatio, element);
+        }
+
+        /// <summary>
+        /// Calculates the clickable point relative to an element based on alignment and offsets.
         /// </summary>
         /// <param name="element">The UI automation uiElement.</param>
         /// <param name="align">The alignment of the clickable point.</param>
@@ -55,9 +92,10 @@ namespace UiaWebDriverServer.Extensions
             scaleRatio = scaleRatio <= 0 ? 1 : scaleRatio;
 
             var location = GetElementLocation(element);
+
+            // Calculate clickable point based on specified alignment
             switch (align.ToUpper())
             {
-                // Calculate clickable point based on specified alignment
                 case "TOPLEFT":
                     return new ClickablePoint
                     {
@@ -121,7 +159,9 @@ namespace UiaWebDriverServer.Extensions
                         YPos = (int)(location.Bottom / scaleRatio) + topOffset
                     };
 
-                // Invoke alternative method if alignment is MIDDLECENTER, or is not recognized
+                case "MIDDLECENTER":
+                    return location.GetMidCenterPoint(scaleRatio);
+
                 default:
                     return InvokeGetClickablePoint(scaleRatio, element);
             }
@@ -144,132 +184,6 @@ namespace UiaWebDriverServer.Extensions
             ExternalMethods.mouse_event(ExternalMethods.MouseEventLeftUp, position.x, position.y, 0, 0);
         }
         
-        /// <summary>
-        /// Try to get a mouse click-able point of the element.
-        /// </summary>
-        /// <param name="element">The Element to get click-able point for.</param>
-        /// <returns>A ClickablePoint object.</returns>
-        public static ClickablePoint GetClickablePoint(this Element element)
-        {
-            return InvokeGetClickablePoint(scaleRatio: 1.0D, element);
-        }
-
-        /// <summary>
-        /// Try to get a mouse click-able point of the element.
-        /// </summary>
-        /// <param name="element">The Element to get click-able point for.</param>
-        /// <param name="scaleRatio">The screen scale ratio e.g., if the scale ratio is 250% this number will be 2.5, if 150% it will be 1.5, etc.</param>
-        /// <returns>A ClickablePoint object.</returns>
-        public static ClickablePoint GetClickablePoint(this Element element, double scaleRatio)
-        {
-            return InvokeGetClickablePoint(scaleRatio, element);
-        }
-
         
-
-        /// <summary>
-        /// Invokes a pattern based MouseClick action on the uiElement. If not possible to use
-        /// pattern, it will use native click.
-        /// </summary>
-        /// <param name="element">The uiElement to click on.</param>
-        /// <remarks>This action will attempt to evaluate the center of the uiElement and click on it.</remarks>
-        public static Element Click(this Element element)
-        {
-            return Click(scaleRatio: 1.0D, element);
-        }
-
-        /// <summary>
-        /// Invokes a pattern based MouseClick action on the uiElement. If not possible to use
-        /// pattern, it will use native click.
-        /// </summary>
-        /// <param name="element">The uiElement to click on.</param>
-        /// <remarks>This action will attempt to evaluate the center of the uiElement and click on it.</remarks>
-        public static Element Click(this Element element, double scaleRatio)
-        {
-            return Click(scaleRatio, element);
-        }
-
-        private static Element Click(double scaleRatio, Element element)
-        {
-
-            // setup conditions
-            var uiElement = element?.UIAutomationElement;
-
-            var pattern = GetElementPattern(uiElement);
-
-            
-            switch (pattern)
-            {
-                case IUIAutomationInvokePattern:
-                    uiElement.Invoke();
-                    break;
-
-                case IUIAutomationExpandCollapsePattern: 
-                    uiElement.ExpandCollapse(); 
-                    break;
-
-                case IUIAutomationSelectionItemPattern:
-                    uiElement.Select();
-                    break;
-
-                default:
-                    var point = InvokeGetClickablePoint(scaleRatio, element);
-                    ExternalMethods.SetPhysicalCursorPos(point.XPos, point.YPos);
-                    InvokeNativeClick();
-                    break;
-            }
-
-            //var isInvoke = uiElement?.GetCurrentPattern(UIA_PatternIds.UIA_InvokePatternId) != null;
-            //var isExpandCollapse = !isInvoke && uiElement?.GetCurrentPattern(UIA_PatternIds.UIA_ExpandCollapsePatternId) != null;
-            //var isSelectable = !isInvoke && !isExpandCollapse && uiElement?.GetCurrentPattern(UIA_PatternIds.UIA_SelectionItemPatternId) != null;
-            //var isCords = !isInvoke && !isExpandCollapse && !isSelectable;
-
-            //// invoke
-            //if (isInvoke)
-            //{
-            //    uiElement.Invoke();
-            //}
-            //else if (isExpandCollapse)
-            //{
-            //    uiElement.ExpandCollapse();
-            //}
-            //else if (isSelectable)
-            //{
-            //    uiElement.Select();
-            //}
-            //else if (isCords)
-            //{
-            //    var point = InvokeGetClickablePoint(scaleRatio, element);
-            //    ExternalMethods.SetPhysicalCursorPos(point.XPos, point.YPos);
-            //    InvokeNativeClick();
-            //}
-
-            // get
-            return element;
-        }
-
-        private static object GetElementPattern(IUIAutomationElement uiElement)
-        {
-            if (uiElement is null)
-            {
-                return null;
-            }
-            var patterns = new[]
-            {
-                UIA_PatternIds.UIA_InvokePatternId,
-                UIA_PatternIds.UIA_ExpandCollapsePatternId,
-                UIA_PatternIds.UIA_SelectionItemPatternId
-            };
-            object patternObject = null;
-            foreach(var pattern in patterns)
-            {
-                patternObject = uiElement.GetCurrentPattern(UIA_PatternIds.UIA_InvokePatternId);
-                if(patternObject != null)
-                {
-                    break;
-                }
-            }
-            return patternObject;
-        }
     }
 }
